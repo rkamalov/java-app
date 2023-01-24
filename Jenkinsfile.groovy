9c0b00076d7b5b93368600d6e2bea75e06d1de7b
rrkamalov <rouslan.kamalov@ya.ru>
2023-01-24 16:15:01 +0300

    initial commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,54 @@
+
+pipeline {
+	agent {
+	
+	}
+
+	stages {
+		stage("Stage1") {
+			steps {
+
+			}
+		} //Stage1
+	
+		stage("Stage2") {
+			steps {
+
+			}
+		} //Stage2
+
+		stage("Stage3") {
+
+		} //Stage3
+			steps {
+
+			}
+		stage("Stage4") {
+			steps {
+
+			}
+		} //Stage4
+
+		stage("Stage5") {
+			steps {
+
+			}
+		} //Stage5
+
+	} //stages
+
+
+	post {
+		always {
+			
+		}
+
+		success {
+		
+		}
+
+		failure {
+
+		}
+	} //post
+} //pipeline
