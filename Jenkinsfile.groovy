3f5eabdd2bf297b5ddd08a5078cdea68e241fe4c
rrkamalov <rouslan.kamalov@ya.ru>
2023-01-24 16:47:26 +0300

    any agent

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,30 +5,30 @@ pipeline {
 	stages {
 		stage("Stage1") {
 			steps {
-
+			echo "Stage 1"
 			}
 		} //Stage1
 	
 		stage("Stage2") {
 			steps {
-
+			echo "Stage 2"
 			}
 		} //Stage2
 
 		stage("Stage3") {
 			steps {
-
+			echo "Stage 3"
 			}
 		} //Stage3
 		stage("Stage4") {
 			steps {
-
+			echo "Stage 4"
 			}
 		} //Stage4
 
 		stage("Stage5") {
 			steps {
-
+			echo "Stage 5"
 			}
 		} //Stage5
 
@@ -37,15 +37,15 @@ pipeline {
 
 	post {
 		always {
-			
+		echo "always"
 		}
 
 		success {
-		
+		echo "success"
 		}
 
 		failure {
-
+		"failure"
 		}
 	} //post
 } //pipeline
