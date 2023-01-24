d58a519b36c574c6723d236a3cc659fcae8b4c53
rrkamalov <rouslan.kamalov@ya.ru>
2023-01-24 22:21:27 +0300

    change pipe

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,7 +6,6 @@ pipeline {
 	stages {
 		stage("Prepare") {
 			steps {
-				sh "export PATH=/Users/rrkamalov/opt/anaconda3/bin:/Users/rrkamalov/opt/anaconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/rrkamalov/bin:/opt/X11/bin:/Library/Apple/usr/bin"
 				git branch: 'main', credentialsId: 'git-cred', url: 'git@github.com:rkamalov/java-app.git'
 			}
 		} //Stage Prepare
@@ -16,17 +15,19 @@ pipeline {
 				// sh 'docker build --file ${WORKSPACE}/docker/Dockerfile --tag gedgrus/java-app:latest .'
 			script {
 				def dockerImage = docker.build("gedgrus/java-app", "-f ${WORKSPACE}/docker/Dockerfile .")
-				docker.withRegistry('', 'dockerhub-cred') {
-					dockerImage.push()
-					dockerImage.push("latest")
-				}
+
 			}
 			}
 		} //Stage Build Image
 
 		stage("Push Image") {
 			steps {
-				sh "docker"
+			script {
+					docker.withRegistry('', 'dockerhub-cred') {
+					dockerImage.push()
+					dockerImage.push("latest")
+				}
+			}
 			}
 		} //Stage3
 		stage("Stage4") {
